4a5039d6e2d8e35b88b4a6b2211f54b5700dd0bf
silvestrevivo <silvestrevivomillan@gmail.com>
2022-07-20 11:34:34 +0200

    combiner working

--- src/args.rs
+++ src/args.rs
@@ -6,15 +6,15 @@ fn get_nth_arg(n: usize) -> String {
 pub struct Args {
     pub image_1: String,
     pub image_2: String,
-    pub  output: String,
+    pub output: String,
 }
 
 impl Args {
     pub fn new() -> Self {
-        Args {
-            image_1: get_nth_arg(1),
-            image_2: get_nth_arg(2),
-            output: get_nth_arg(3),
-        }
+      Args {
+        image_1: get_nth_arg(1),
+        image_2: get_nth_arg(2),
+        output: get_nth_arg(3),
+      }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use image::{io::Reader, DynamicImage, ImageFormat, GenericImageView, imageops::F
 #[derive(Debug)]
 enum ImageDataErrors {
     DifferentImageFormats,
-    BufferTooSmall
+    BufferTooSmall,
 }
 
 
@@ -25,13 +25,14 @@ impl FloatingImage {
             width,
             height,
             data: buffer,
-            name
+            name,
         }
     }
 
     fn set_data(&mut self, data: Vec<u8>) -> Result<(), ImageDataErrors> {
+        // If the previously assigned buffer is too small to hold the new data
         if data.len() > self.data.capacity() {
-            return Err(ImageDataErrors::BufferTooSmall);
+          return Err(ImageDataErrors::BufferTooSmall);
         }
         self.data = data;
         Ok(())
@@ -42,16 +43,18 @@ impl FloatingImage {
 fn main() -> Result<(), ImageDataErrors> {
 
     let args = Args::new();
-    let (image_1, image_format_1) = find_image_from_path(args.image_1);
-    let (image_2, image_format_2) = find_image_from_path(args.image_2);
+    let (image_1, image_1_format) = find_image_from_path(args.image_1);
+    let (image_2, image_2_format) = find_image_from_path(args.image_2);
 
 
 
-    if image_format_1 != image_format_2 {
+
+    if image_1_format != image_2_format {
         return Err(ImageDataErrors::DifferentImageFormats);
     }
 
-    let (image_1, image_2) = standarise_size(image_1, image_2);
+
+    let (image_1, image_2) = standardise_size(image_1, image_2);
 
     let mut output = FloatingImage::new(image_1.width(), image_1.height(), args.output);
 
@@ -59,7 +62,15 @@ fn main() -> Result<(), ImageDataErrors> {
 
     output.set_data(combined_data)?;
 
-    image::save_buffer_with_format(output.name, &output.data, output.width, output.height, image::ColorType::Rgb8, image_format_1).unwrap();
+    image::save_buffer_with_format(
+        output.name,
+        &output.data,
+        output.width,
+        output.height,
+        image::ColorType::Rgba8,
+        image_1_format,
+      )
+      .unwrap();
 
     Ok(())
 }
@@ -73,26 +84,25 @@ fn find_image_from_path(path: String) -> (DynamicImage, ImageFormat) {
     (image, image_format)
 }
 
-fn get_smallest_dimension(dim_1: (u32, u32), dim_2: (u32, u32)) -> (u32, u32) {
+fn get_smallest_dimensions(dim_1: (u32, u32), dim_2: (u32, u32)) -> (u32, u32) {
     let pix_1 = dim_1.0 * dim_1.1;
     let pix_2 = dim_2.0 * dim_2.1;
 
     return if pix_1 < pix_2 { dim_1 } else { dim_2 };
-}
+  }
 
-fn standarise_size(image_1: DynamicImage, image_2: DynamicImage) -> (DynamicImage, DynamicImage) {
-    let (width, height) = get_smallest_dimension(image_1.dimensions(), image_2.dimensions());
-
-    println!("width: {width}, height: {height}\n");
+fn standardise_size(image_1: DynamicImage, image_2: DynamicImage) -> (DynamicImage, DynamicImage) {
+    let (width, height) = get_smallest_dimensions(image_1.dimensions(), image_2.dimensions());
+    println!("width: {}, height: {}\n", width, height);
 
     if image_2.dimensions() == (width, height) {
-        (image_1.resize_exact(width, height, Triangle), image_2)
+      (image_1.resize_exact(width, height, Triangle), image_2)
     } else {
-        (image_1, image_2.resize_exact(width, height, Triangle))
+      (image_1, image_2.resize_exact(width, height, Triangle))
     }
-}
+  }
 
-fn combine_images(image_1: DynamicImage, image_2: DynamicImage) -> Vec<u8> {
+  fn combine_images(image_1: DynamicImage, image_2: DynamicImage) -> Vec<u8> {
     let vec_1 = image_1.to_rgba8().into_vec();
     let vec_2 = image_2.to_rgba8().into_vec();
 
@@ -100,28 +110,29 @@ fn combine_images(image_1: DynamicImage, image_2: DynamicImage) -> Vec<u8> {
 }
 
 fn alternate_pixels(vec_1: Vec<u8>, vec_2: Vec<u8>) -> Vec<u8> {
+    // A Vec<u8> is created with the same length as vec_1
     let mut combined_data = vec![0u8; vec_1.len()];
 
     let mut i = 0;
     while i < vec_1.len() {
-        if i % 8 == 0 {
-            combined_data.splice(1..=i + 3, set_rgba(&vec_1, i, i + 3));
-        } else {
-            combined_data.splice(1..=i + 3, set_rgba(&vec_2, i, i + 3));
-        }
-        i += 4;
+      if i % 8 == 0 {
+        combined_data.splice(i..=i + 3, set_rgba(&vec_1, i, i + 3));
+      } else {
+        combined_data.splice(i..=i + 3, set_rgba(&vec_2, i, i + 3));
+      }
+      i += 4;
     }
+
     combined_data
 }
 
 
 fn set_rgba(vec: &Vec<u8>, start: usize, end: usize) -> Vec<u8> {
     let mut rgba = Vec::new();
-
     for i in start..=end {
         let val = match vec.get(i) {
-            Some(d) => *d,
-            None => panic!("Index out of bounds")
+        Some(d) => *d,
+        None => panic!("Index out of bounds"),
         };
         rgba.push(val);
     }
